703af24ac7811196c7d343f81f5b537b61874525
ayaelbadrawey <ayaelbadrawey@gmail.com>
2021-09-25 17:07:15 +0200

    1st component added, the header

---
+++ e-commerce/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Paper from '@mui/material/Paper';
+import './style.css';
+
+function Header() {
+    return (
+        <div className="div1">
+            <Paper className="head" >
+                <h1 className="siteName">Our e-commerce store</h1>
+            </Paper>
+        </div>
+    )
+}
+
+export default Header
