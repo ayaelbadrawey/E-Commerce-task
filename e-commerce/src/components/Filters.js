e00760ddf6ec360dfcb0bfb7dd68fe3df8c6f91d
ayaelbadrawey <ayaelbadrawey@gmail.com>
2021-09-26 01:11:11 +0200

    new component, ProceFilter

--- e-commerce/src/components/Filters.js
+++ e-commerce/src/components/Filters.js
@@ -1,5 +1,6 @@
 import { Paper } from '@mui/material'
 import React from 'react'
+import PriceFilter from './PriceFilter'
 import './style.css'
 
 function Filters() {
@@ -7,7 +8,7 @@ function Filters() {
         <div>
             <Paper className="filters" >
             <p className="filter">Filters</p>
-               
+                <PriceFilter />
             </Paper>
         </div>
     )
